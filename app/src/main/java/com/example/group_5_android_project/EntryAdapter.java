ef235cd973bdf10d27235d891e87c8cbb2bccbc0
Jorazon <oskari.pahkala@gmail.com>
2020-12-08 13:31:36 +0200

    entry adapter now supports filtering based on date selected in calendar
    plus icon color is based on theme primary color
    
    "no entries yet" text is shown if no entries are on the selected date

--- app/src/main/java/com/example/group_5_android_project/EntryAdapter.java
+++ app/src/main/java/com/example/group_5_android_project/EntryAdapter.java
@@ -1,28 +1,47 @@
 package com.example.group_5_android_project;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 //https://guides.codepath.com/android/Using-an-ArrayAdapter-with-ListView
 
-public class EntryAdapter extends ArrayAdapter<CalendarEntry> {
+public class EntryAdapter extends ArrayAdapter<CalendarEntry> implements Filterable {
 
-    private Activity activity;
+    private final List<CalendarEntry> data;
+    private List<CalendarEntry> filtered;
+
+    private EntryFilter filter;
 
     public EntryAdapter(Context context, List<CalendarEntry> objects) {
-        super(context, 0, objects);
+        super(context, 0);
+        data = objects;
+        filtered = objects;
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent){
+    public int getCount() {
+        return filtered.size();
+    }
+
+    public CalendarEntry getItem(int position) {
+        return filtered.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
         CalendarEntry entry = getItem(position);
 
         if (convertView == null) {
@@ -33,7 +52,7 @@ public class EntryAdapter extends ArrayAdapter<CalendarEntry> {
         TextView description = convertView.findViewById(R.id.entryDescription);
 
         //set icon image
-        switch (entry.getType()){
+        switch (entry.getType()) {
             case LIIKUNTA:
                 icon.setImageResource(R.drawable.ic_entry_exercise);
                 break;
@@ -45,5 +64,58 @@ public class EntryAdapter extends ArrayAdapter<CalendarEntry> {
         description.setText(entry.toString());
 
         return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if(filter == null)
+        {
+            filter=new EntryFilter();
+        }
+
+        return filter;
+    }
+
+    private class EntryFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults result = new FilterResults();
+
+            if(constraint != null && constraint.length()>0) {
+
+                ArrayList<CalendarEntry> resultingEntries = new ArrayList<>();
+
+                long msInDay = 86400000;
+
+                long date = Long.parseLong((String) constraint);
+
+                Date selected = new Date(date - 1);
+                Date selectedPlus1 = new Date(date + msInDay);
+
+                for (CalendarEntry entry : data) {
+                    Date time = entry.getTime();
+                    if(time.after(selected) && time.before(selectedPlus1)){
+                        resultingEntries.add(entry);
+                    }
+                }
+
+                result.count = resultingEntries.size();
+                result.values = resultingEntries;
+            }
+            else{
+                result.count = data.size();
+                result.values = data;
+            }
+
+            return result;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filtered = (List<CalendarEntry>) results.values;
+            notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/group_5_android_project/MainActivity.java
+++ app/src/main/java/com/example/group_5_android_project/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.group_5_android_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.CalendarView;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     CalendarView calendar;
     ListView entryListView;
 
+    long calendarSelectedDate;
+
+    EntryAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +31,32 @@ public class MainActivity extends AppCompatActivity {
         calendar = findViewById(R.id.calendarView);
         entryListView = findViewById(R.id.list_entries);
 
+        calendarSelectedDate = calendar.getDate();
 
         for(int i = 0; i < 10; ++i){
-            entries.addEntry(new CalendarEntry(new EntryType[]{EntryType.RUOKA,EntryType.LIIKUNTA}[new Random().nextInt(2)] ,new Date(calendar.getDate()),"test entry " + i));
+            entries.addEntry(new CalendarEntry(new EntryType[]{EntryType.RUOKA,EntryType.LIIKUNTA}[new Random().nextInt(2)] ,new Date(calendarSelectedDate),"test entry " + i));
         }
 
         //set adapter for listview
-        entryListView.setAdapter(new EntryAdapter(
+        adapter = new EntryAdapter(
                 this,
                 entries.getEntries()
-        ));
+        );
+
+        entryListView.setAdapter(adapter);
+
+        calendar.setOnDateChangeListener(
+            (view, year, month, dayOfMonth) -> {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year,month,dayOfMonth-1);//jostain syystä pitää laittaa -1 että päivä osuu oikein
+                setFilter(calendar.getTimeInMillis());
+            }
+        );
+    }
+
+    private void setFilter(long TimeInMillis){
+        calendarSelectedDate = TimeInMillis;
+        adapter.getFilter().filter(Long.toString(TimeInMillis));
+        findViewById(R.id.noentriesflag).setVisibility((entryListView.getCount() == 0) ? View.INVISIBLE : View.VISIBLE);
     }
 }
